808bbfc4814063204a2ed76829096dacb23e8d5e
Calvin Rodrigues <56112227+calvin-ux@users.noreply.github.com>
2022-07-18 10:38:38 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,12 @@
+"""
+Area of circle program 
+@author: Calvin
+"""
+
+print("Area of Cirle Program")
+pi = 3.141    
+# alternate pi value take 3.141592653589793238
+r = float(input("Enter the value of radius :"))
+area = pi*r*r                
+# area = pi*r**2 (alternate code)
+print("Area of Circle is:", area)
