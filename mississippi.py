858e30fd2ca4c38056e1f9166e51b19744606b24
Calvin Rodrigues <56112227+calvin-ux@users.noreply.github.com>
2022-08-03 21:48:08 +0530

    Update mississippi.py

--- mississippi.py
+++ mississippi.py
@@ -3,28 +3,6 @@
 
 @author: Calvin
 """
-"""
-letter = {
-    "m" : 0,
-    "s" : 0,
-    "p" : 0,
-    "i" : 0
-    }
-
-def most_frequent(word):
-   for i in word:
-       if i == "m":
-           letter["m"] = letter["m"] + 1
-       elif i == "p":
-           letter["p"] = letter["p"] + 1
-       elif i == "s":
-           letter["s"] = letter["s"] + 1
-       elif i == "i":
-           letter["i"] = letter["i"] + 1
-           print(letter)
-
-most_frequent("mississippi")
-"""
 
 letter = {
     "m" : 0,
@@ -51,15 +29,3 @@ def most_frequent(word="default args"):
    print("Descending Output: ", desc_values)   
 
 most_frequent("mississippi")
-
-
-
-
-
-
-
-letter = {"Math": 34, "Science": 12, "English": 89, "Physics": 8}
-change_do = ((value, key) for (key,value) in letter.items())
-desc_values = sorted(change_do, reverse=True)
-print(desc_values)
-
